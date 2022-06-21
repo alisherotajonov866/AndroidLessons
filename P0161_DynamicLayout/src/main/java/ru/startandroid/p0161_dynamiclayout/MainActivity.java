fd99a1a9c9aae093c4af52a65e7a51a54721bfdd
alisherotajonov866 <alisherotajonov866@gmail.com>
2022-06-21 17:20:21 +0500

    DynamicLayout

--- P0161_DynamicLayout/src/main/java/ru/startandroid/p0161_dynamiclayout/MainActivity.java
+++ P0161_DynamicLayout/src/main/java/ru/startandroid/p0161_dynamiclayout/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -29,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
         Button btn = new Button(this);
         btn.setText("Button");
         linLayout.addView(btn, lpView);
+
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+
+        Button btn1 = new Button(this);
+        btn1.setText("Button1");
+        linLayout.addView(btn1, leftMarginParams);
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button btn2 = new Button(this);
+        btn2.setText("Button2");
+        linLayout.addView(btn2, rightGravityParams);
     }
 }
