680678479bc846d638d0c7a6ea89b3125b656303
alisherotajonov866 <alisherotajonov866@gmail.com>
2022-07-23 11:31:58 +0500

    Lesson_36_37

--- P0361_SQLiteQuery/src/main/java/ru/startandroid/p0361_sqlitequery/MainActivity.java
+++ P0361_SQLiteQuery/src/main/java/ru/startandroid/p0361_sqlitequery/MainActivity.java
@@ -1,14 +1,196 @@
 package ru.startandroid.p0361_sqlitequery;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    String name[] = {"Xitoy","AQSH","Braziliya","Rossiya","Yaponiya","Germaniya","Misr","Italiya","Fransiya","Kanada"} ;
+    int people[] = {1400, 311, 195, 142, 128, 82, 80, 60, 66, 35} ;
+    String region[] = {"Osiya","Amerika","Amerika","Yevropa","Osiya","Yevropa","Afrika",
+    "Yevropa","Yevropa","Amerika"} ;
+
+    Button btnAll, btnFunc, btnPeople, btnSort, btnGroup, btnHaving;
+    EditText etFunc, etPeople, etRegionPeople;
+    RadioGroup rgSort;
+
+    DBHelper dbHelper ; // DBHelper classi bilan bizni bog'lovchi,shu class tipli global o'zgaruvchi
+    SQLiteDatabase db ; // bazaga yangi narsa qo'shmoqchi bo'lsak ,bazaning onCreate usuli bilan bizni bog'lovchi vosita
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAll = (Button) findViewById(R.id.btnAll);
+        btnAll.setOnClickListener(this);
+
+        btnFunc = (Button) findViewById(R.id.btnFunc);
+        btnFunc.setOnClickListener(this);
+
+        btnPeople = (Button) findViewById(R.id.btnPeople);
+        btnPeople.setOnClickListener(this);
+
+        btnSort = (Button) findViewById(R.id.btnSort);
+        btnSort.setOnClickListener(this);
+
+        btnGroup = (Button) findViewById(R.id.btnGroup);
+        btnGroup.setOnClickListener(this);
+
+        btnHaving = (Button) findViewById(R.id.btnHaving);
+        btnHaving.setOnClickListener(this);
+
+        etFunc = (EditText) findViewById(R.id.etFunc);
+        etPeople = (EditText) findViewById(R.id.etPeople);
+        etRegionPeople = (EditText) findViewById(R.id.etRegionPeople);
+
+        rgSort = (RadioGroup) findViewById(R.id.rgSort);
+
+        dbHelper = new DBHelper(this) ; // DBHelper classining obyektini e'lon qildik.
+
+        db = dbHelper.getWritableDatabase() ; // bazaga ulanish
+        // so'rov yubordik
+        Cursor c = db.query("mytable",null,null,null,null,null,null) ;
+
+        if (c.getCount()==0){ // baza teshkirildi // bazadan bizga malumot olib keladigan "quti"
+            ContentValues cv = new ContentValues() ;// bazaga bizning kodimizni olib boradigan "quti"
+            for (int i=0;i<10;i++){ // bo'sh bazaga malumot yozilyapti
+                cv.put("name",name[i]);
+                cv.put("people",people[i]);
+                cv.put("region",region[i]);
+                Log.d(LOG_TAG, "id = " + db.insert("mytable", null, cv));
+            }
+        }
+        c.close(); // soro'v yuborish to'xtatildi
+        dbHelper.close(); // bazaga ulanish to'xtatildi.
+
+        onClick(btnAll); // bu kod btnAll bosilmasa ham ,dastur ishga tushganda , barcha yozuvlarni ko'rsatish uchun
+
+    }
+
+    @SuppressLint("Range")
+    @Override
+    public void onClick(View v) {
+
+        db = dbHelper.getWritableDatabase() ; // bazaga ulandik
+
+        // edittext dagi matnlar o'zgaruchilarga o'zlashtirildi
+        String sFunc = etFunc.getText().toString();
+        String sPeople = etPeople.getText().toString();
+        String sRegionPeople = etRegionPeople.getText().toString();
+
+        // query(so'rovlar) usulida qo'llaniladigan o'zgaruchilarni tavsifladik
+        String[] columns = null;
+        String selection = null;
+        String[] selectionArgs = null;
+        String groupBy = null;
+        String having = null;
+        String orderBy = null;
+        String distinct = null ;
+
+        //cursor ni tavsifladik
+        Cursor c = null ;
+
+        switch (v.getId()){
+            case R.id.btnAll:
+                Log.d(LOG_TAG, "--- Barcha yozuvlar: ---");
+                //
+                c = db.query("mytable",null, null, null, null, null, null) ;
+                break;
+            case R.id.btnFunc:
+                Log.d(LOG_TAG, "--- Funksiya " + sFunc + " ---");
+                columns = new String[]{sFunc} ; // string tipli massiv, o'zgaruchisi - etFunc ga kelgan malumot
+                //
+                c = db.query("mytable",columns,null, null, null, null, null) ;
+                break;
+            case R.id.btnPeople: //aholisi ekranda kiritilgan raqamdan ko'p bo'lgan mamlakatlarni ko'rsatadi
+                Log.d(LOG_TAG, "--- Alohi soni  " + sPeople + " dan ko'pi");
+                selection  = "people > ?" ;
+                selectionArgs = new String[]{sPeople} ;
+                c = db.query("mytable",null,selection,selectionArgs,null,null,null) ;
+                break;
+            case  R.id.btnGroup: // mamlakatlarni mintaqalar bo'yicha guruhlash va umumiy aholini ko'rsatish
+                Log.d(LOG_TAG,"Mintaqalar bo'yicha aholi soni") ;
+                columns = new String[] {"region","sum(people) as people"} ;
+                groupBy = "region" ; // guruhlash region bo'yicha bo'lishini anglatadi
+                c = db.query("mytable",columns,null,null,groupBy,null,null) ;
+                break;
+            case R.id.btnHaving: // aholi soni ko'rsatilgan sondan ko'p bo'lgan hududlarni ko'rsatadi
+                Log.d(LOG_TAG, "Aholi soni " + sRegionPeople+" dan ko'p bo'lgan hududlar");
+                columns = new String[] {"region","sum(people) as people"} ;// shu "" ichindakila sql so'rovlari manimcha,shuni bir go'rishim garak
+                groupBy = "region" ;
+                having = "sum(people) >"+sRegionPeople ;
+                c = db.query("mytable",columns,null,null,groupBy,having,null) ;
+                break;
+            case R.id.btnSort:
+                switch (rgSort.getCheckedRadioButtonId()){// qaysi radio button bosilgani aniqlandi
+                    case R.id.rName:
+                        Log.d(LOG_TAG,"Nomi bo'yicha saralandi") ;
+                        orderBy = "name" ; // saralash alifbo tartibida
+                        break;
+                    case R.id.rPeople:
+                        Log.d(LOG_TAG,"Aholi bo'yicha saralandi") ;
+                        orderBy = "people" ; // saralash o'sish tartibida
+                        break;
+                    case R.id.rRegion:
+                        Log.d(LOG_TAG,"Region bo'yicha saralandi") ;
+                        orderBy = "region" ; // saralash alifbo tartibida
+                        break;
+                }
+                c = db.query("mytable",null, null, null, null, null, orderBy) ;
+                break;
+        }
+
+        if (c!=null){// bazada yozuvlar borligi aniqlanyabti
+            if (c.moveToFirst()){
+                String str ;
+                do {
+                    str = "" ;
+                    for (String cn:c.getColumnNames()){// ustun nomlari 'cn' o'zgaruvchisiga yuklatilyabti
+                        // har bir maydon id si 'str' o'zgaruvchisiga olinyabti
+                        str = str.concat(cn+" = "+c.getString(c.getColumnIndex(cn))+"; ") ;
+                    }
+                    Log.d(LOG_TAG,str) ;
+                }while(c.moveToNext()) ;
+            }
+            c.close(); // so'rov yuborish to'xtatildi
+        }else
+            Log.d(LOG_TAG,"Cursor bo'sh") ;
+        db.close(); // bazaga ulanish uzuldi
+    }
+
+    class DBHelper extends SQLiteOpenHelper{
+
+
+        public DBHelper(@Nullable Context context) {
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- Baza yaratildi! ---");
+                db.execSQL("CREATE TABLE mytable (" +  "id INTEGER PRIMARY KEY AUTOINCREMENT," + "name TEXT," + "people INTEGER," +
+                        "region TEXT" +  ") ");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        }
     }
 }
---
+++ P0371_SQLiteInnerJoin/src/main/java/ru/startandroid/p0371_sqliteinnerjoin/MainActivity.java
@@ -0,0 +1,134 @@
+package ru.startandroid.p0371_sqliteinnerjoin;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs" ;
+
+    // bazadagi malumotlar bilan ishlashimiz uchun, shu massivdagi elementlarni bazaga o'qib olib ulardan foydalanib turamiz.
+    // bular 1-jadval uchun
+    int position_id [] = {1,2,3,4} ;
+    String position_name [] = {"Direktor","Dasturchi","Buggalter","Ohran"} ;
+    int position_salary [] = {15000, 13000, 10000, 8000} ;
+
+    //2-jadval uchun
+    String people_name[] = {"Alisher","Diyor","Fayzulla","Jasur","Baxromjon","Sardor","Ibrohim","Zafar"} ;
+    int people_posid[] = {2, 3, 2, 2, 3, 1, 2, 4 } ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DBHelper dbh = new DBHelper(this) ;// Malumotlar bazasini boshqarish uchun DBHelper classidan obyekt oldik
+        SQLiteDatabase db = dbh.getWritableDatabase() ;// dbh obyekti orqali bazaga ulanildi
+
+        Cursor c ;
+
+        Log.d(LOG_TAG, "--- Table position ---");
+        // position jadvalini ko'rsatish uchun so'rov yuborildi
+        c = db.query("position",null, null, null, null, null, null) ;
+        logCursor(c) ;
+        c.close();// so'rov to'xtatildi
+        Log.d(LOG_TAG, "--- ---");
+
+        Log.d(LOG_TAG, "--- Table people ---");
+        c = db.query("people",null, null, null, null, null, null) ;
+        logCursor(c) ;
+        c.close();
+        Log.d(LOG_TAG, "--- ---");
+
+        Log.d(LOG_TAG, "--- INNER JOIN with rawQuery---");
+        String sqlQuery = "select PL.name as Name, PS.name as Position, salary as Salary "
+                + "from people as PL "
+                + "inner join position as PS "
+                + "on PL.posid = PS.id "
+                + "where salary > ?";
+        c = db.rawQuery(sqlQuery,new String[]{"12000"}) ;
+        logCursor(c) ;
+        c.close();
+        Log.d(LOG_TAG, "--- ---");
+
+        Log.d(LOG_TAG, "--- INNER JOIN with query---");
+        String table = "people as PL inner join position as PS on PL.posid = PS.id" ;
+        String columns[] = {"Pl.name as Name", "PS.name as Position", "salary as Salary"} ;
+        String selection = "salary < ?";
+        String[] selectionArgs = {"12000"};
+        c = db.query(table, columns, selection, selectionArgs, null, null, null);
+        logCursor(c);
+        c.close();
+        Log.d(LOG_TAG, "--- ---");
+
+        db.close();
+    }
+
+    void logCursor(Cursor c) {
+        if (c != null) {
+            if (c.moveToFirst()) {
+                String str;
+                do {
+                    str = "";
+                    for (String cn : c.getColumnNames()) {
+                        str = str.concat(cn + " = " + c.getString(c.getColumnIndex(cn)) + "; ");
+                    }
+                    Log.d(LOG_TAG, str);
+                } while (c.moveToNext());
+            }
+        } else
+            Log.d(LOG_TAG, "Cursor is null");
+    }
+
+    class DBHelper extends SQLiteOpenHelper{
+
+        public DBHelper(@Nullable Context context) {
+            super(context, "mytable", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG,"Baza yaratildi!");
+
+            ContentValues cv = new ContentValues() ; // bazaga malumot olib boradigan "quti" dan obyekt olindi
+
+            // 1-baza yaratildi, lekin u hali bo'sh
+            db.execSQL("CREATE TABLE position (" + "id integer primary key autoincrement," + "name text," + "salary integer" + ") ; ");
+
+            // 1-bazaga massivlardan malumot qo'shildi
+            for (int i=0;i<position_id.length;i++){
+                cv.clear(); // quti bo'shatildi
+                cv.put("id",position_id[i]);
+                cv.put("name",position_name[i]);
+                cv.put("id",position_salary[i]);
+                // qutidagi malumotlarni 'position' jadvaliga yuborildi
+                db.insert("position",null,cv) ;
+            }
+
+            // 2-baza yaratildi, lekin u hali bo'sh
+            db.execSQL("CREATE TABLE people (" + "id integer primary key autoincrement," + "name text," + "posid integer" + ") ; ");
+
+            // 2-bazaga massivlardan malumot qo'shildi
+            for (int i=0;i<people_name.length;i++){
+                cv.clear(); // quti bo'shatildi
+                cv.put("name",people_name[i]);
+                cv.put("posid",people_posid[i]);
+                // qutidagi malumotlarni 'position' jadvaliga yuborildi
+                db.insert("people",null,cv) ;
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        }
+    }
+}
